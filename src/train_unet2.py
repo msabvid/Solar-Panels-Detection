29a9ec437a2a77aa37fd9ef04525f6f54fd217fb
Marc Sabaté <marcsv87@gmail.com>
2018-03-04 21:59:01 +0000

    Update train_unet2.py

--- src/train_unet2.py
+++ src/train_unet2.py
@@ -1,6 +1,5 @@
 #TODO: change all paths to arguments
 import sys
-#sys.path.append('/Users/msabate/Projects/CityFinancial/src')
 sys.path.append('/floydhub/CityFinancial/Solar-Panels-Detection/src')
 
 import argparse
@@ -55,15 +54,7 @@ count = 0
 
 
 my_transform = transforms.Compose([
-    transforms.Scale(256)])#,
-    #transforms.ToTensor()])#,
-#    Normalize([.485, .456, .406], [.229, .224, .225]),
-#])
-#target_transform = Compose([
-#    CenterCrop(256),
-#    ToLabel(),
-#    Relabel(255, 22),
-#])
+    transforms.Scale(256)])
 
 class Unet2(nn.Module):
 
@@ -113,54 +104,30 @@ class Unet2(nn.Module):
         self.last = nn.Conv2d(32,2,kernel_size=1)
 
     def forward(self, x):
-        #print('x size is {}'.format(x.size()))
         d1 = self.D1(x)
-        #print('d1 size is {0}'.format(d1.size()))
-        #print('d1 torch element_size is {}'.format(d1.element_size()))
         d1_pool = nn.MaxPool2d(kernel_size=2, stride=2)(d1)
-        #print('d1_pool size is {}'.format(d1_pool.size()))
         d2 = self.D2(d1_pool)
-        #print('d2 size is {}'.format(d2.size()))
         d2_pool = nn.MaxPool2d(kernel_size=2, stride=2)(d2)
-        #print('d2_pool size is {}'.format(d2_pool.size()))
         d3 = self.D3(d2_pool)
-        #print('d3 size is {}'.format(d3.size()))
         d3_pool = nn.MaxPool2d(kernel_size=2, stride=2)(d3)
-        #print('d3_pool size is {}'.format(d3_pool.size()))
         d4 = self.D4(d3_pool)
-        #print('d4 size is {}'.format(d4.size()))
         d4_pool = nn.MaxPool2d(kernel_size=2, stride=2)(d4)
-        #print('d4_pool size is {}'.format(d4_pool.size()))
         b = self.B(d4_pool)
-        #print('b size is {}'.format(b.size()))
         u4_input = self.U4_input(b)
-        #print('u4_input size is {}'.format(u4_input.size()))
         u4_input = u4_input.contiguous()
-        #print('hola')
         u4 = self.U4(torch.cat((d4,u4_input),1))
-        #print('u4 size is {}'.format(u4.size()))
         u3_input = self.U3_input(u4)
-        #print('u3_input size is {}'.format(u3_input.size()))
         u3 = self.U3(torch.cat((d3,u3_input),1))
-        #print('u3 size is {}'.format(u3.size()))
         u3 = u3.contiguous()
         u2_input = self.U2_input(u3)
-        #print('u2_input size is {}'.format(u2_input.size()))
         u2 = self.U2(torch.cat((d2,u2_input),1))
-        #print('u2 size is {}'.format(u2.size()))
         u1_input = self.U1_input(u2)
         u1_input = u1_input.contiguous()
-        #print('u1_input size is {}'.format(u1_input.size()))
         u1 = self.U1(torch.cat((d1,u1_input),1))
         y = self.last(u1)
-        #print('y size is {}'.format(y.size()))
-        #print('y element_size is {}'.format(y.element_size()))
         y = y.view(batch_size,2,-1).contiguous()
-        #print('y size is {}'.format(y.size()))
         y = y.transpose(1,2).contiguous()
-        #print('y size is {}'.format(y.size()))
         y = y.view(-1,2).contiguous()
-        #print('y size is {}'.format(y.size()))
         return y
 
 def main():
@@ -437,12 +404,7 @@ def accuracy(output, target, topk=(1,)):
     """Computes the precision@k for the specified values of k"""
     batch_size = target.size(0)
     target = target.view(-1)
-    #output = output.view(batch_size,2,-1).contiguous()
-    #output = output.transpose(1,2).contiguous()
-    #output = output.view(-1,2).contiguous()
-
     target = target.view(-1).contiguous()
-
     _, pred = output.topk(1, 1, True, True)
     #pred = pred.t()
     #print(pred)
@@ -496,5 +458,4 @@ def accuracy2(output, target, topk=(1,)):
 
 
 if __name__ == '__main__':
-    print('hola')
     main()
