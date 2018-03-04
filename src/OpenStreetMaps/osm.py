a858a1664915bd17f01008b167f8fdc42f807403
marcsv87 <marcsv87@gmail.com>
2018-03-04 21:45:19 +0000

    folders re-organised

--- src/OpenStreetMaps/osm.py
+++ src/OpenStreetMaps/osm.py
@@ -14,10 +14,11 @@ from scipy import stats
 import fiona
 from glob import glob
 import re
+import sklearn
+from collections import Counter
+import itertools
 
 
-
-os.getcwd()
 
 
 PROJECT_DIR = '/Users/msabate/Projects/CityFinancial'
@@ -25,7 +26,10 @@ SENTINEL_WORKING_DIR = join(PROJECT_DIR, 'data', 'Sentinel', 'working')
 SENTINEL_INPUT_DIR = join(PROJECT_DIR, 'data', 'Sentinel', 'input')
 WGS84_DIR = join(PROJECT_DIR, 'data', 'Sentinel', 'working',)
 SHAPEFILE_PATH = '/Users/msabate/Projects/CityFinancial/data/OSM/england-latest-free/gis.osm_water_a_free_1.shp'
+SHAPEFILE_PATH = '/Users/msabate/Projects/CityFinancial/data/OSM/scotland-latest-free/gis.osm_landuse_a_free_1.shp'
 
+os.getcwd()
+os.chdir(PROJECT_DIR)
 
 with rasterio.open('S2A_MSIL1C_20170222T104031_N0204_R008_T31TDF_20170222T104801_resampled_RGB.tif') as src:
     src.width
@@ -35,8 +39,7 @@ with rasterio.open('S2A_MSIL1C_20170222T104031_N0204_R008_T31TDF_20170222T104801
     src.crs
     src.count
     
-    
-    
+
     
 with rasterio.open('S2A_MSIL1C_20170222T104031_N0204_R008_T31TDF_20170222T104801_resampled_RGB.tif') as src:
    a = src.read()
@@ -46,6 +49,11 @@ with rasterio.open('S2A_MSIL1C_20170222T104031_N0204_R008_T31TDF_20170222T104801
    print(src.crs)
    print(src.count)
     
+
+
+with rasterio.open('data/Sentinel/working/working.tif') as src:
+    a = src.read()
+
     
 r, g, b = a[0,:,:], a[1,:,:], a[2,:,:]
 
@@ -54,8 +62,7 @@ img = np.stack([r,g,b])
 tiff.imshow(img)
 
 
-geotiff_path = join(SENTINEL_WORKING_DIR, 'S2A_MSIL1C_20170222T104031_N0204_R008_T31TDF_20170222T104801_resampled_RGB.tif')
-geotiff_path = join(SENTINEL_WORKING_DIR, 'working.tif')
+
 
 def reproject_dataset(geotiff_path):
     """Project a GeoTIFF to the W
@@ -80,7 +87,7 @@ def reproject_dataset(geotiff_path):
             'height': height
         })
 
-        satellite_img_name = os.path.basename(geotiff_path)
+        satellite_img_name, _ = os.path.splitext(os.path.basename(geotiff_path))
         out_file_name = "{}_wgs84.tif".format(satellite_img_name)
         out_path = os.path.join(WGS84_DIR, out_file_name)
         with rasterio.open(out_path, 'w', **kwargs) as dst:
@@ -93,7 +100,7 @@ def reproject_dataset(geotiff_path):
                     dst_transform=transform,
                     dst_crs=dst_crs,
                     resampling=rasterio.warp.Resampling.nearest)
-
+    
         return rasterio.open(out_path), out_path
 
 
@@ -103,6 +110,10 @@ with fiona.open(SHAPEFILE_PATH) as shapefile:
     geometries = [feature['geometry'] for feature in shapefile]
     water_features = np.concatenate((water_features, geometries), axis=0)
     
+
+geotiff_path = join(SENTINEL_WORKING_DIR, 'S2A_MSIL1C_20170222T104031_N0204_R008_T31TDF_20170222T104801_resampled_RGB.tif')
+geotiff_path = join(SENTINEL_WORKING_DIR, 'working.tif')
+
 raster_dataset, wgs84_path = reproject_dataset(geotiff_path)
 
 bitmap_image = rasterio.features.rasterize(
@@ -132,6 +143,75 @@ newlist = filter(r.match, a)
 
 
 
+# Features in land-use shape files from Scotland
+SHAPEFILE_SCOTLAND = '/Users/msabate/Projects/CityFinancial/data/OSM/scotland-latest-free/'
+l_shp = glob(SHAPEFILE_SCOTLAND + '*.shp')
+
+land_use = '/Users/msabate/Projects/CityFinancial/data/OSM/scotland-latest-free/gis.osm_landuse_a_free_1.shp'
+
+list_classes = []
+src = fiona.open(land_use)
+for p in src:
+    list_classes.append(p['properties']['fclass'])
+
+src.close()
+
+counter = Counter(list_classes)
+counter.most_common()
+
+
+# we create land-use features for scotland
+mask_feature = 'residential'
+SHAPEFILE_PATH = '/Users/msabate/Projects/CityFinancial/data/OSM/scotland-latest-free/gis.osm_landuse_a_free_1.shp'
+features = np.empty((0,))
+with fiona.open(SHAPEFILE_PATH) as shapefile:
+    geometries = [feature['geometry'] for feature in shapefile if feature['properties']['fclass']==mask_feature]
+    features = np.concatenate([features, geometries], axis=0)
+
+geotiff_path = join(SENTINEL_WORKING_DIR, 'edinburgh.tif')
+raster_dataset, wgs84_path = reproject_dataset(geotiff_path)
+
+bitmap_image = rasterio.features.rasterize(
+    ((g, 255) for g in features),
+    out_shape=raster_dataset.shape,
+    transform=raster_dataset.transform)
+
+
+tiff.imsave(join(SENTINEL_WORKING_DIR, 
+                 os.path.splitext(os.path.basename(geotiff_path))[0] + '_mask_'+mask_feature+'.tif'), bitmap_image)
+
+
+# FEATURES in roads
+roads_shp = '/Users/msabate/Projects/CityFinancial/data/OSM/scotland-latest-free/gis.osm_roads_free_1.shp'
+
+list_classes = []
+src = fiona.open(roads_shp)
+for p in src:
+    list_classes.append(p['properties']['fclass'])
+
+src.close()
+
+counter = Counter(list_classes)
+counter.most_common()
+
+# we create roads features for scoltands
+SHAPEFILE_PATH = '/Users/msabate/Projects/CityFinancial/data/OSM/scotland-latest-free/gis.osm_roads_free_1.shp'
+features = np.empty((0,))
+with fiona.open(SHAPEFILE_PATH) as shapefile:
+    geometries = [feature['geometry'] for feature in shapefile]
+    features = np.concatenate([features, geometries], axis=0)
+
+geotiff_path = join(SENTINEL_WORKING_DIR, 'edinburgh.tif')
+raster_dataset, wgs84_path = reproject_dataset(geotiff_path)
+
+bitmap_image = rasterio.features.rasterize(
+    ((g, 255) for g in features),
+    out_shape=raster_dataset.shape,
+    transform=raster_dataset.transform)
+
+
+tiff.imsave(join(SENTINEL_WORKING_DIR, 
+                 os.path.splitext(os.path.basename(geotiff_path))[0] + '_mask_roads.tif'), bitmap_image)
 
 
 
